0904fd45983eecf39510fe336bec376500949e6f
misrokhah <32092489+misrokhah@users.noreply.github.com>
2017-12-28 13:22:53 +0700

    Add files via upload

---
+++ Kelas.java
@@ -0,0 +1,17 @@
+public class Kelas{
+
+    public kelas(){
+        infoProgram();
+    }
+
+    public void infoProgram(){
+        System.out.println("------- CONTOH PROGRAM DATA KELAS -------\n");
+    }
+
+    public void infoKelas(){
+        System.out.println("------- DATA KELAS -------\n");
+    }
+
+    
+
+}
